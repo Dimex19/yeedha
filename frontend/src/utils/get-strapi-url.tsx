f6e30bd0da8aedc4295de27a51fec7bf27eee327
Dimex19 <azeezabolaji06@gmail.com>
2025-11-25 07:27:00 +0100

    privacy policy added

--- frontend/src/utils/get-strapi-url.tsx
+++ frontend/src/utils/get-strapi-url.tsx
@@ -1,4 +1,4 @@
 export function getStrapiURL() {
-    return import.meta.env.VITE_STRAPI_API_URL || 'http://localhost:1337';
-    // return import.meta.env.VITE_STRAPI_API_URL || 'https://typical-rainbow-6582065928.strapiapp.com';
+    // return import.meta.env.VITE_STRAPI_API_URL || 'http://localhost:1337';
+    return import.meta.env.VITE_STRAPI_API_URL || 'https://typical-rainbow-6582065928.strapiapp.com';
   }
